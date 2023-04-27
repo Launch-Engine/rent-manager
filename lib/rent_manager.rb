89b1fa6271ad857033b923f448c1bc10c4364a63
Wes Hays <weshays@gmail.com>
2023-04-26 18:46:39 -0700

    Updated rent_manger

--- lib/rent_manager.rb
+++ lib/rent_manager.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 require 'rubygems'
-require 'json'
-require 'typhoeus'
-require 'awrence'
-require 'plissken'
+# require 'json'
+# require 'typhoeus'
+# require 'awrence'
+# require 'plissken'
 
 require_relative 'rent_manager/version'
 
